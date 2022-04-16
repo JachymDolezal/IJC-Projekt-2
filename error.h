f7a08d150983843d36c83a8943d2dcf8288d6faa
JachymD <jachym.dolezal@gmail.com>
2022-04-16 23:40:58 +0200

    Create error.h

---
+++ error.h
@@ -0,0 +1,11 @@
+/*
+ * error.h
+ * IJC-DU1, příklad a), 18.3.2022
+ * Autor: Jáchym Doležal, FIT
+ * Login: xdolez0c
+ * Přeloženo: gcc 11.2
+ */
+
+void warning_msg(const char* fmt, ...);
+
+void error_exit(const char* fmt, ...);
